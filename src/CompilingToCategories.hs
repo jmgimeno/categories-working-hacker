4b64e44494d0ca9bb9058b2ec98532d11c31bb4d
Juan Manuel Gimeno Illa <jmgimeno@gmail.com>
2018-04-11 19:34:28 +0200

    An interpreter for cartesian categories and an instance for functions

---
+++ src/CompilingToCategories.hs
@@ -0,0 +1,84 @@
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
+
+module CompilingToCategories where
+
+import Prelude hiding (id, (.), curry, uncurry, const)
+
+-- Classes 
+
+infixr 9 .
+class Category k where
+  id :: a `k` a
+  (.) :: (b `k` c) -> (a `k` b) -> (a `k` c)
+  
+infixr 3 |*|
+class Category k => Cartesian k where
+  (|*|) :: (a `k` c) -> (a `k` d) -> (a `k` (c, d))
+  exl :: (a, b) `k` a
+  exr :: (a, b) `k` b
+  
+-- How to generalize () for any category?
+class Category k => Terminal k where
+  it :: a `k` ()
+  
+class Cartesian k => Closed k where
+  applyC :: ((a -> b), a) `k` b
+  curry :: ((a, b) `k` c) -> (a `k` (b -> c))
+  uncurry :: (a `k` (b -> c)) -> ((a, b)  `k` c)
+  
+class Terminal k => ConstCat k b where
+  unitArrow :: b -> (() `k` b)
+ 
+const :: ConstCat k b => b -> (a `k` b)
+const b = unitArrow b . it
+ 
+class Cartesian k => BoolCat k where
+  notC :: Bool `k` Bool
+  andC, orC :: (Bool, Bool) `k` Bool
+  ifteC :: (Bool, (a, a)) `k` a
+  
+class NumCat k a where
+  negateC :: a `k` a
+  addC, mulC :: (a, a) `k` a
+
+class OrdCat k a where
+  gteC :: (a, a) `k` Bool
+  
+-- Instances
+
+instance Category (->) where
+  id = \x -> x
+  g . f = \x -> g (f x)
+  
+instance Cartesian (->) where
+  f |*| g = \x -> (f x, g x)
+  exl = \(a, b) -> a
+  exr = \(a, b) -> b
+  
+instance Terminal (->) where
+  it = \a -> ()
+  
+instance Closed (->) where
+  applyC (f, a) = f a
+  curry f = \a b -> f (a, b)
+  uncurry f = \(a, b) -> f a b
+
+instance ConstCat (->) b where
+  unitArrow b = \() -> b
+  
+instance BoolCat (->) where
+  notC = not
+  andC = uncurry (&&)
+  orC  = uncurry (||)
+  ifteC = \(b, (t, e)) -> if b then t else e
+  
+instance Num a => NumCat (->) a where
+  negateC = negate
+  addC = uncurry (+)
+  mulC = uncurry (*)
+
+instance Ord a => OrdCat (->) a where 
+  gteC = uncurry (>=)
